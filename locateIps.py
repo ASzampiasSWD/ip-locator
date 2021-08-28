bd052b3201c9acba0cd5b8c51c38b2e5d92744e0
Amanda Szampias <aszampiasswd@gmail.com>
2021-08-28 11:39:58 -0400

    VPN detection added

--- locateIps.py
+++ locateIps.py
@@ -3,15 +3,30 @@
 import os.path
 import requests
 import json
-import subprocess
 import csv
 import simplekml
 
 arGeoResults = []
+bVPNApi = False
+strAPIKey = ""
 
 if os.path.exists("ipFile.txt") == False:
   print("Error. Please create a file called ipFile.txt and supply IP Addresses separated by new line breaks.")
   exit()
+
+if os.path.exists("iphub-info.txt") == True:
+  bVPNApi = True
+  strAPIKey = open("iphub-info.txt").readline().strip()
+  print("iphub-info.txt API exists. API will be used.")
+
+def getVPNStatus(ip):
+  strLink = "http://v2.api.iphub.info/ip/" + ip.strip()
+  headers={"X-Key" : strAPIKey}  
+  response = requests.get(strLink, headers=headers, auth=None)
+  strResponse = response.content.decode("utf-8")
+  jsonResponse = json.loads(strResponse)
+  return jsonResponse
+
 
 ipFile = open("ipFile.txt")
 for ip in ipFile:
@@ -19,14 +34,20 @@ for ip in ipFile:
   response = requests.get("http://www.geoplugin.net/json.gp?ip=" + ip)
   strResponse = response.content.decode("utf-8")
   jsonResponse = json.loads(strResponse)
+  jsonResponse["isp"] = "";
+  jsonResponse["block"] = "";
   if (response.status_code == 200):
     arGeoResults.append(jsonResponse)
+    if (bVPNApi == True):
+      jsonVPN = getVPNStatus(ip)
+      jsonResponse["isp"] = jsonVPN["isp"]
+      jsonResponse["block"] = jsonVPN["block"]
   else:
     print(ip + " had an error. Status Code " + response.status_code)
 
 geoFile = open("geo-coords.csv", "w")
 statsFile = open("geo-stats.csv", "w")
-strStatsTitle = "IP Address, Latitude, Longitude, City, Region, Country, Timezone\n"
+strStatsTitle = "IP Address, Latitude, Longitude, City, Region, Country, Timezone, ISP, Blocked\n"
 statsFile.write(strStatsTitle)
 
 for jsonGeo in arGeoResults:
@@ -34,7 +55,7 @@ for jsonGeo in arGeoResults:
   strGeo = strGeoTitle + ',' + jsonGeo["geoplugin_latitude"] + ',' + jsonGeo["geoplugin_longitude"]    
   strStats = jsonGeo["geoplugin_request"].strip() + ',' + jsonGeo["geoplugin_latitude"] + ',' + jsonGeo["geoplugin_longitude"] + ',' + \
              jsonGeo["geoplugin_city"] + ',' + jsonGeo["geoplugin_region"] + ',' + jsonGeo['geoplugin_countryName'] + ',' + \
-             jsonGeo["geoplugin_timezone"]
+             jsonGeo["geoplugin_timezone"] + ',' + jsonGeo["isp"] + ',' + str(jsonGeo["block"])
   print(strGeo)
   geoFile.write(strGeo + "\n")
   statsFile.write(strStats + "\n")
